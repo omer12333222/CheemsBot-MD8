fba7b742dacf75703ee6e8125a46cf086368f156
omer12333222 <124527373+omer12333222@users.noreply.github.com>
2023-09-19 23:35:25 +0500

    settings.js

--- settings.js
+++ settings.js
@@ -21,12 +21,12 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['923331675335'] //ur owner number
+global.ownernomer = "923331675335" //ur owner number2
+global.ownername = "Umer Khan" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Kabul, Afghanistan " //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
